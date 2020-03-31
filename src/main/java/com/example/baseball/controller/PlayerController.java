89ad018c962278b49ce6581086b660fadc18902c
1859kcal <62131335+1859kcal@users.noreply.github.com>
2020-03-31 11:50:34 +0900

    2020/03/31 11:50

--- src/main/java/com/example/baseball/controller/PlayerController.java
+++ src/main/java/com/example/baseball/controller/PlayerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,7 +45,7 @@ public class PlayerController {
 	public String show(@PathVariable Long id, Model model) {
 		Player player = playerService.findOne(id);
 		model.addAttribute("player", player);
-		return "player/show";
+		return "players/show";
 	}
 
 	@PostMapping
@@ -54,6 +55,13 @@ public class PlayerController {
 	}
 
 	@PutMapping("{id}")
+	public String update(@PathVariable Long id, @ModelAttribute Player player) {
+		player.setId(id);
+		playerService.save(player);
+		return "redirect:/players";
+	}
+
+	@DeleteMapping("{id}")
 	public String destroy(@PathVariable Long id) {
 		playerService.delete(id);
 		return "redirect:/players";
